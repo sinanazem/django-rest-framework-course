73ad04e2df38bbec2e60c349f7d8d576965cce11
sinanazem <sinanaazem@gmail.com>
2024-04-18 09:27:04 +0330

    add authentication

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,8 +1,14 @@
 from django.urls import path, include
 from .views import UserRegister
+from rest_framework.authtoken import views as authtoken_views
 
 app_name = 'accounts'
 
 urlpatterns = [
     path('register/', UserRegister.as_view()),
+]
+
+
+urlpatterns += [
+    path('api-token-auth/', authtoken_views.obtain_auth_token)
 ]
