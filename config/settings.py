39d7b98725ab87abd398f5b3ffadb534c544125e
sinanazem <sinanaazem@gmail.com>
2024-04-18 09:42:34 +0330

    add permissions

--- config/settings.py
+++ config/settings.py
@@ -128,6 +128,6 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': [
-        'rest_framework.authentication.SessionAuthentication',
+        'rest_framework.authentication.TokenAuthentication',
     ]
 }
