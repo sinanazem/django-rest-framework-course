eca255f8c26817af7764b14098c7b2ea11d1df95
sinanazem <sinanaazem@gmail.com>
2024-04-18 08:33:40 +0330

    add validators to serializers

--- accounts/serializers.py
+++ accounts/serializers.py
@@ -1,13 +1,29 @@
 from rest_framework import serializers
 
+def clean_email(value):
+    if 'admin' in value.lower():
+        raise serializers.ValidationError("email can not include `admin` ")
+    return value
+
 
 class UserRegisterSerializer(serializers.Serializer):
     username = serializers.CharField(required=True)
-    email = serializers.EmailField(required=True)
+    email = serializers.EmailField(required=True, validators=[clean_email,])
     password = serializers.CharField(required=True, write_only=True)
+    confirm_password = serializers.CharField(required=True, write_only=True)
     
     
+    def validate_username(self, value):
+        
+        if value.lower() == 'admin':
+            raise serializers.ValidationError('username can not be `admin` ')
+        return value
     
+    def validate(self, data):
+        
+        if data['password'] != data['confirm_password']:
+            raise serializers.ValidationError('passwords are not match!')
+        return data
 
 
 
